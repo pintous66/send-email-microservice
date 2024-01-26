b8382153ae3a87fe9da5796bfa5ad454f20bcacd
Pinto <rodrigomnp1104@gmail.com>
2024-01-26 03:56:58 +0000

    Initial commit

---
+++ src/main/java/com/example/sendMail/SendMailApplication.java
@@ -0,0 +1,12 @@
+package com.example.sendMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SendMailApplication.class, args);
+	}
+
+}
